4f072497984ca88d97d64a3a072daf12911b5f53
Abdullah Al Noman <abdullah.noman@advisori.de>
2022-08-17 17:53:32 +0200

    Expose Entity ids

--- backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/abdullahalnoman/springbootecommerce/repository/model/ProductCategory.java
+++ backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/abdullahalnoman/springbootecommerce/repository/model/ProductCategory.java
@@ -1,10 +1,15 @@
 package com.abdullahalnoman.springbootecommerce.repository.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "product_category")
+@Getter
+@Setter
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
