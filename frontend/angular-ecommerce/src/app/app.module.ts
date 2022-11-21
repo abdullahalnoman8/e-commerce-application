cd69d13539f2be66a6d6201b1c5901b0d58bd5ad
Abdullah Al Noman <opucse8@gmail.com>
2022-11-21 03:01:18 +0100

    Basic Pagination

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategroyMenuComponent } from './components/product-categroy-menu/product-categroy-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   {path:'products/:id',component:ProductDetailsComponent},
@@ -31,7 +32,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -18,7 +18,7 @@ export class ProductService {
 
   }
   
-
+  // Get all the products using find By category Id
   getProductList(theCategoryId:number):Observable<Product[]>{
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
@@ -28,13 +28,14 @@ export class ProductService {
     
    }
 
+   // Get all the category ids 
    getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
   }
 
-  // Search Products method
+  // Search Products method for search header in the page
   searchProducts(thekeyword: string) : Observable<Product[]> {
      // need to build URL based on category id 
      const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${thekeyword}`
@@ -45,11 +46,13 @@ export class ProductService {
   
   
 
+  // Return all the products 
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
 
   // Get product method
   getProduct(theProductId: number) : Observable<Product> {
@@ -58,11 +61,36 @@ export class ProductService {
     
     return this.httpClient.get<Product>(productUrl);
   }
+
+  /** 
+   * Get all the products that will contain pagination information
+   * 
+   * **/
+  getProductListPaginate(
+  thePage:number,
+  thePageSize:number,
+  theCategoryId: number  
+  ):Observable<GetResponseProducts>{
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`+
+    `&page=${thePage}&size=${thePageSize}`;
+
+    console.log('search URL: '+ searchUrl)
+    
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+    
+   }
 }
+
 interface GetResponseProducts{
   _embedded:{
-    products: Product[];
-    
+    products: Product[];  
+  },
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
   }
  }
  interface GetResponseProductCategory{
